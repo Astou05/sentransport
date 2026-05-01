17bc7f7b2846e908f08e4946b85b98acc3b1ab63
Astou05 <asta4631@gmail.com>
2026-05-01 17:56:31 +0000

    Setup initial SenTransport avec Header

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <h1 classeName="header-titre">SenTransport</h1>
+            <p classeName="header-soustitre">Votre guide du transport en commun</p>
+        </header>
+    );
+}
+export default Header;
